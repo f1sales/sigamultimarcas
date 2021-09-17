2221f359d216fbf23fff0956e3230a2d691faa76
Thiago Youssef <thiagodyoussef@gmail.com>
2021-09-17 16:33:23 -0300

    Sigamultimarcas parser

--- lib/sigamultimarcas.rb
+++ lib/sigamultimarcas.rb
@@ -9,12 +9,30 @@ module Sigamultimarcas
   class Error < StandardError; end
   class F1SalesCustom::Email::Source
     def self.all
-      # ...
+      [
+        {
+          email_id: 'website',
+          name: 'Website'
+        }
+      ]
     end
   end
   class F1SalesCustom::Email::Parser
     def parse
-      # ...
+      parsed_email = @email.body.colons_to_hash
+      source = F1SalesCustom::Email::Source.all[0]
+
+      {
+        source: {
+          name: source[:name]
+        },
+        customer: {
+          name: parsed_email['de'],
+          phone: parsed_email['telwhatsapp'],
+          email: parsed_email['email']
+        },
+        message: parsed_email['carro_de_interesse']
+      }
     end
   end
 end
